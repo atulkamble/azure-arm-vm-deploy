28bd8ce5bcd52a7f7badac1b55307010036c3c1e
Atul Kamble <atul_kamble@hotmail.com>
2025-11-29 09:14:56 +0530

    first commit

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+az group create --name MyResourceGroup --location eastus
+az deployment group create --resource-group MyResourceGroup --template-file azuredeploy.json --parameters vmName=myVM adminUsername=atul adminPassword=Ethans@123
